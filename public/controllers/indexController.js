cccb88919c61c0fbfd754d5349a89ac04fc96ecf
ochiba0227 <newgroundofswim@gmail.com>
2016-05-21 03:01:24 +0900

    anglar-bootstrap 連携まで

--- public/controllers/indexController.js
+++ public/controllers/indexController.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('spicyApp1', []);
+var myApp = angular.module('spicyApp1', ['ui.bootstrap']);
 myApp.controller('SpicyCtrl', ['$scope', function($scope){
     $scope.spice = 'very';
     $scope.chiliSpicy = function() {
@@ -7,4 +7,9 @@ myApp.controller('SpicyCtrl', ['$scope', function($scope){
     $scope.jalapenoSpicy = function() {
         $scope.spice = 'jalapeño';
     };
+    $scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
 }]);
