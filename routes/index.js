73645826eeb8b841fc85f1556a2c6690521882f5
ochiba0227 <newgroundofswim@gmail.com>
2016-05-22 22:44:59 +0900

    スキーマの設定まで

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var global_var = require('./var.js');
-var db = global_var.getDB();
+var competition = global_var.getCompetitionModel();
+var entry = global_var.getEntryModel();
 
-// ToDoスキーマを定義する
-var Schema = db.Schema;
-var todoSchema = new Schema({
-  isCheck     : {type: Boolean, default: false},
-  text        : String,
-  createdDate : {type: Date, default: Date.now},
-  limitDate   : Date
-});
-db.model('Todo', todoSchema);
-
-// /todoにGETアクセスしたとき、ToDo一覧を取得するAPI
-router.get('/todo', function(req, res) {
-  var Todo = db.model('Todo');
-  // すべてのToDoを取得して送る
-  Todo.find({}, function(err, todos) {
-    res.send(todos);
-  });
-});
-
-
-// /todoにPOSTアクセスしたとき、ToDoを追加するAPI
-router.post('/todo', function(req, res) {
-  console.log("inininininin");
-  var name = req.body.name;
-  var limit = req.body.limit;
-  // ToDoの名前と期限のパラーメタがあればMongodbに保存
-  console.log("inininininin");
-  if(name && limit) {
-    var Todo = db.model('Todo');
-    var todo = new Todo();
-    todo.text = name;
-    todo.limitDate = limit;
-    todo.save();
-    console.log(todo)
-
-    res.send(true);
-  } else {
-    res.send(false);
-  }
-});
+  //   var makeCompetition = new competition();
+  //   makeCompetition.title = "test_third";
+  //   makeCompetition.save();
+  //   // console.log(makeCompetition)
+  //     competition.find({}, function(err, comp) {
+  //   console.log(comp);
+  // });
+      // var makeEntry = new entry();
+      // makeEntry.programId = "test_third";
+      // makeEntry.save();
+      // console.log(makeCompetition)
+        entry.find({}, function(err, comp) {
+      console.log(comp);
+    });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/var.js
+++ routes/var.js
@@ -2,8 +2,70 @@ var conf = require('config');
 var mongoose = require('mongoose');
 var db = mongoose.connect(conf.mongodb.url);
 
+// スキーマを定義する
+var Schema = db.Schema;
+
+// 試合
+var competitionSchema = new Schema({
+  title        : String,                          //試合タイトル
+  description  : String,                          //試合説明
+  day : {type: Date, default: Date.now},          //実施日
+  createdDate : {type: Date, default: Date.now},  //作成日
+  updatedDate : {type: Date, default: Date.now}   //更新日
+});
+var competitionModel = db.model(conf.mongodb.schemaName.competiton, competitionSchema);
+
+// プログラム
+var programSchema = new Schema({
+  competitionId : String,                         //competitionテーブルのid
+  programNum : Number,                           //プログラム番号
+  distance : Number,                             //距離(m)
+  title        : String,                          //自由形など競技名
+  description  : String,                          //競技内容
+  isRelay : {type:Boolean,default:false},         //リレーならtrue
+  createdDate : {type: Date, default: Date.now},  //作成日
+  updatedDate : {type: Date, default: Date.now},  //更新日
+});
+var programModel = db.model(conf.mongodb.schemaName.program, programSchema);
+
+// エントリー
+var entrySchema = new Schema({
+  programId : String,                             //programテーブルのid
+  entryData : {                                   //エントリーデータをjson形式で格納
+    userId:[String],                              //ユーザIDを配列形式で格納
+    entryTime:Date,                               //エントリータイム
+  },
+  recordId:String,                               //recordテーブルのid
+  createdDate : {type: Date, default: Date.now},  //作成日
+  updatedDate : {type: Date, default: Date.now},  //更新日
+});
+var entryModel = db.model(conf.mongodb.schemaName.entry, entrySchema);
+
+// 記録
+var recordSchema = new Schema({
+  entryId : String,                                //entryテーブルのID
+  programId : String,                             //programテーブルのid
+  record : Date,                                  //タイム
+  createdDate : {type: Date, default: Date.now},  //作成日
+  updatedDate : {type: Date, default: Date.now},  //更新日
+});
+var recordModel = db.model(conf.mongodb.schemaName.record, recordSchema);
+
+// ユーザ
+var userSchema = new Schema({
+  userId   : Number,                              //user固有のid
+  department : String,                             //学科(M,E,S,I,C)
+  graduateYear : Date,                             //卒業(見込み)年度
+  createdDate : {type: Date, default: Date.now},  //作成日
+  updatedDate : {type: Date, default: Date.now},  //更新日
+});
+var userModel = db.model(conf.mongodb.schemaName.user, userSchema);
+
 module.exports = {
-    getDB: function(){
-      return db
+    getCompetitionModel: function(){
+      return competitionModel
+    },
+    getEntryModel: function(){
+      return entryModel
     }
 }
